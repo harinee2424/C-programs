2d7c26d6734a61d9ec5009dfb738a6350d05866f
harinee2424 <harinee.m.2018.cse@ritchennai.edu.in>
2021-06-11 19:38:00 +0530

    biggest number

---
+++ conditional statements/big3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter a");
+    scanf("%d",&a);
+    printf("enter b");
+    scanf("%d",&b);
+    printf("enter c");
+    scanf("%d",&c);
+    if(a > b && a > c){
+        printf("%d is big",a);
+    }
+    else if(b>c){
+        printf("%d is big",b);
+    }
+    else{
+          printf("%d is big",c);
+    }
+
+}
