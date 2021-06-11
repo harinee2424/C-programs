9c4c38841ae1fffbb869ff8052af2e6ce47a5e95
harinee2424 <harinee.m.2018.cse@ritchennai.edu.in>
2021-06-11 13:12:47 +0530

    commit 1

---
+++ conditional statements/positive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>=0){
+        printf("it is positive");
+    }
+    else if(num<=0){
+        printf("it is negative");
+    }
+    else{
+        printf("it is zero");
+    }
+    return 0;
+}
