043b938904f1ac9c1c134b523305d8a27d6a1fe2
harinee2424 <harinee.m.2018.cse@ritchennai.edu.in>
2021-06-11 19:08:38 +0530

    sum

---
+++ conditional statements/sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,num,c;
+    c=a+b;
+    scanf("%d%d",&a,&b);
+    if(num%2==0){
+        printf("it is even");
+    }
+    else{
+        printf("it is odd");
+    }
+    return 0;
+
+}
