69e6c1b24e6d25d77ad0a344e50d5824826ddcef
harinee2424 <harinee.m.2018.cse@ritchennai.edu.in>
2021-06-11 18:55:13 +0530

    greater no

---
+++ conditional statements/greatest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    scanf("%d%d",&num1,&num2);
+    if(num1>num2){
+        printf("num 1 is greater");
+    }
+    else{
+        printf("num 2 is greater");
+    }
+    return 0;
+}
