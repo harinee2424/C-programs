417ab71cd58b7752257e3fa72de487c3a697bf0e
harinee2424 <harinee.m.2018.cse@ritchennai.edu.in>
2021-06-29 11:15:37 +0530

    swapping

---
+++ conditional statements/swap.c
@@ -0,0 +1,15 @@
+//swapping using two variables using pointers
+#include<stdio.h>
+void swap(int* x,int* y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int main(){
+    int a = 10, b = 20;
+    swap(&a,&b);
+    printf("a is %d and b is %d\n",a,b);
+    return 0;
+
+}
